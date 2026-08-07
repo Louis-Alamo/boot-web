c6dc2ee2ffc53092ff4e23b985309d3a44e87679
louis_serrano <louis_serrano@fragua.com.mx>
2026-08-07 12:43:25 -0600

    Feat: pendiente dialog

---
+++ src/components/confirm-dialog/confirm-dialog.js
@@ -0,0 +1,43 @@
+
+
+let btnCancelar;
+let btnAceptar;
+let pTexto;
+let hTitulo;
+let contenedorIcono;
+
+
+
+
+/**
+ * 
+ * @param {*} config 
+ * Contiene toda la configuracion para el componente
+ */
+export function mostrarModal (config){
+
+    inicializarVariables();
+
+}
+
+function inicializarVariables () {
+    btnAceptar = document.getElementById("btn-aceptar");
+    btnCancelar = document.getElementById("btn-cancelar");
+    contenedorIcono = document.getElementById("contenedor-icono");
+    hTitulo = document.getElementById("titulo");
+    pTexto = document.getElementById("mensaje");
+
+}
+
+
+function construirInterfaz() {
+
+    
+
+}
+
+
+function configurarBotones () {
+
+
+}
